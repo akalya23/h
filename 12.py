8506205d84e7683ca54e0f48655f39d2cb06d42f
akalya23 <akalyaammu26@gmail.com>
2019-06-07 14:41:20 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,11 +1,11 @@
-s1,s2=input().split()
+s1,m=input().split()
 su=0
-if len(s1)>len(s2):
-    s1,s2=s2,s1
+if len(s1)>len(m):
+    s1,sm=sm,s1
 q=0
 while q<len(s1):
-    su+=(ord(s2[q])-ord(s1[q]))
+    su+=(ord(m[q])-ord(s1[q]))
     q+=1
-for q in range(q,len(s2)):
-    su+=ord(s2[q])-ord('a')+1
+for q in range(q,len(sm)):
+    su+=ord(m[q])-ord('a')+1
 print(su)
